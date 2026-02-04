671cb758c868c5b07c6adac4ffc0f5115454dcaa
zynx <gsgsdgsdgsdgsdg2@gmail.com>
2026-02-04 20:53:16 +0100

    f

--- main.py
+++ main.py
@@ -1,4 +1,20 @@
 import requests
+import time
+import random
+import string
+
+banner = """
+  ____  _   _ ____  _____ ____  _   _    __        __         _     ____  _   _ 
+ / ___|| | | |  _ \| ____|  _ \| | | |   \ \      / /__  _ __| | __/ ___|| | | |
+ \___ \| |_| | |_) |  _| | |_) | |_| |    \ \ /\ / / _ \| '__| |/ /\___ \| |_| |
+  ___) |  _  |  __/| |___|  __/|  _  |     \ V  V / (_) | |  |   <  ___) |  _  |
+ |____/|_| |_|_|   |_____|_|   |_| |_|      \_/\_/ \___/|_|  |_|\_\|____/|_| |_|
+                                                                                    
+"""
+print(banner)
+
+
+
 
 
 print(requests.get("https://offsets.ntgetwritewatch.workers.dev/offsets.hpp").text)
