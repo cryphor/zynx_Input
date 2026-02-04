fba96a332f69ebc71eb76d88576354e9f32c53f2
zynx <gsgsdgsdgsdgsdg2@gmail.com>
2026-02-04 21:36:00 +0100

    added 2 stuff

--- main.py
+++ main.py
@@ -1,25 +1,60 @@
-from re import X
 import requests
-import time
-import random
-import string
+import os
+import json
+from rich import print, pretty
+from rich.table import Table
+from rich.panel import Panel
+from rich.box import HEAVY
+from rich.console import Console
+from rich import print, panel
 
-console.clear()
+# logo
+logo = """[bold cyan]
+███╗   ██╗███████╗███████╗███████╗███████╗███████╗███████╗
+████╗  ██║██╔════╝██╔════╝██╔════╝██╔════╝██╔════╝██╔════╝
+██╔██╗ ██║█████╗  ███████╗█████╗  ███████╗█████╗  ███████╗
+██║╚██╗██║██╔══╝  ╚════██║██╔══╝  ╚════██║██╔══╝  ╚════██║
+██║ ╚████║███████╗███████║███████╗███████║███████╗███████║
+╚═╝  ╚═══╝╚══════╝╚══════╝╚══════╝╚══════╝╚══════╝╚══════╝
 
-banner = """
-  ____  _   _ ____  _____ ____  _   _    __        __         _     ____  _   _ 
- / ___|| | | |  _ \| ____|  _ \| | | |   \ \      / /__  _ __| | __/ ___|| | | |
- \___ \| |_| | |_) |  _| | |_) | |_| |    \ \ /\ / / _ \| '__| |/ /\___ \| |_| |
-  ___) |  _  |  __/| |___|  __/|  _  |     \ V  V / (_) | |  |   <  ___) |  _  |
- |____/|_| |_|_|   |_____|_|   |_| |_|      \_/\_/ \___/|_|  |_|\_\|____/|_| |_|
-                                                                                    
 """
-print(banner)
 
-x = {x: f"{x}. View Offsets" for x in range(1, 2)}
+# main
+while True:
+    os.system("NESSES")
+    os.system("cls")
+    print(logo)
+    print("[bold cyan][1]. View Offsets[/bold cyan]")
+    print("[bold cyan][2]. IP Lookup[/bold cyan]")
+    
+    
+    # -----------------
+    quit = print("[bold red]Type 'quit' to exit the program")
+    x = input(f"[$]: ")
 
-input(f"[$] Choose: ")
+    if x == "1":
+        print(requests.get("https://offsets.ntgetwritewatch.workers.dev/offsets.hpp").text)
+        pause = input("Press [Enter] to continue... ")
+        
+    if x == "2":
+        print("IP LOOKUP\n")
+        ip = input("Enter IP Address: ")
+        if not ip:
+            print("This aint a ip address bro")
+        else:
+            os.system("cls")
+            r = requests.get(f"https://ipinfo.io/json/{ip}")
+            data = r.json()
 
-
-if x == "1":
-    print(requests.get("https://offsets.ntgetwritewatch.workers.dev/offsets.hpp").text)
+            table = Table(title="IP Information", box=HEAVY)
+            table.add_column("Property", style="cyan")
+            table.add_column("Value", style="magenta")
+            for key, value in data.items():
+                table.add_row(key, str(value))
+            console = Console()
+            console.print(table)
+            pause = input("Press [Enter] to continue... ")
+    
+    if x == "quit":
+        os.system("cls")
+        break
