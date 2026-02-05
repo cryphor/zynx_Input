5b9e2581a3030b3fdb7eab9eae087dae3883b566
zynx <gsgsdgsdgsdgsdg2@gmail.com>
2026-02-05 17:27:37 +0100

    Fixed ip lookup way more comming tmrw

--- main.py
+++ main.py
@@ -9,32 +9,33 @@ from rich.console import Console
 from rich import print, panel
 
 # logo
-logo = """[bold cyan]
-███╗   ██╗███████╗███████╗███████╗███████╗███████╗███████╗
-████╗  ██║██╔════╝██╔════╝██╔════╝██╔════╝██╔════╝██╔════╝
-██╔██╗ ██║█████╗  ███████╗█████╗  ███████╗█████╗  ███████╗
-██║╚██╗██║██╔══╝  ╚════██║██╔══╝  ╚════██║██╔══╝  ╚════██║
-██║ ╚████║███████╗███████║███████╗███████║███████╗███████║
-╚═╝  ╚═══╝╚══════╝╚══════╝╚══════╝╚══════╝╚══════╝╚══════╝
+logo = """[bold red]
+███████╗██╗   ██╗███╗   ██╗██╗  ██╗        ██╗███╗   ██╗██████╗ ██╗   ██╗████████╗
+╚══███╔╝╚██╗ ██╔╝████╗  ██║╚██╗██╔╝        ██║████╗  ██║██╔══██╗██║   ██║╚══██╔══╝
+  ███╔╝  ╚████╔╝ ██╔██╗ ██║ ╚███╔╝         ██║██╔██╗ ██║██████╔╝██║   ██║   ██║   
+ ███╔╝    ╚██╔╝  ██║╚██╗██║ ██╔██╗         ██║██║╚██╗██║██╔═══╝ ██║   ██║   ██║   
+███████╗   ██║   ██║ ╚████║██╔╝ ██╗███████╗██║██║ ╚████║██║     ╚██████╔╝   ██║   
+╚══════╝   ╚═╝   ╚═╝  ╚═══╝╚═╝  ╚═╝╚══════╝╚═╝╚═╝  ╚═══╝╚═╝      ╚═════╝    ╚═╝   
 
 """
 
 # main
 while True:
-    os.system("NESSES")
+    os.system("ZYNX_INPUT")
     os.system("cls")
     print(logo)
-    print("[bold cyan][1]. View Offsets[/bold cyan]")
-    print("[bold cyan][2]. IP Lookup[/bold cyan]")
+    print("[bold red][1]. View Offsets[/bold red]")
+    print("[bold red][2]. IP Lookup[/bold red]")
     
     
     # -----------------
-    quit = print("[bold red]Type 'quit' to exit the program")
+    quit = print("[bold yellow]Type 'quit' to exit the program")
     x = input(f"[$]: ")
 
     if x == "1":
+        os.system("cls")
         print(requests.get("https://offsets.ntgetwritewatch.workers.dev/offsets.hpp").text)
-        pause = input("Press [Enter] to continue... ")
+        pause = input("Press enter to continue... ")
         
     if x == "2":
         print("IP LOOKUP\n")
@@ -43,17 +44,20 @@ while True:
             print("This aint a ip address bro")
         else:
             os.system("cls")
-            r = requests.get(f"https://ipinfo.io/json/{ip}")
+            r = requests.get(f"https://ipinfo.io/{ip}")
             data = r.json()
-
+            
+            # table
             table = Table(title="IP Information", box=HEAVY)
             table.add_column("Property", style="cyan")
-            table.add_column("Value", style="magenta")
+            value = table.add_column("Info", style="magenta")
+            
+            # rows
             for key, value in data.items():
                 table.add_row(key, str(value))
             console = Console()
             console.print(table)
-            pause = input("Press [Enter] to continue... ")
+            pause = input("Press enter to continue... ")
     
     if x == "quit":
         os.system("cls")
