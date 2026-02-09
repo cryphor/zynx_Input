aee1c47ad0432102ba6603cd99ea30a92e84e30a
zynx <gsgsdgsdgsdgsdg2@gmail.com>
2026-02-09 01:10:25 +0100

    Worst update on github ✌️

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ from rich.console import Console
 from rich import print, panel
 
 # logo
-logo = """[bold red]
+logo = """[bold purple]
 ███████╗██╗   ██╗███╗   ██╗██╗  ██╗        ██╗███╗   ██╗██████╗ ██╗   ██╗████████╗
 ╚══███╔╝╚██╗ ██╔╝████╗  ██║╚██╗██╔╝        ██║████╗  ██║██╔══██╗██║   ██║╚══██╔══╝
   ███╔╝  ╚████╔╝ ██╔██╗ ██║ ╚███╔╝         ██║██╔██╗ ██║██████╔╝██║   ██║   ██║   
@@ -24,12 +24,13 @@ while True:
     os.system("ZYNX_INPUT")
     os.system("cls")
     print(logo)
-    print("[bold red][1]. View Offsets[/bold red]")
-    print("[bold red][2]. IP Lookup[/bold red]")
+    print("[bold purple][1]View Offsets")
+    print("[bold purple][2]IP Lookup")
+    print("[bold purple][3]Lookup")
     
     
     # -----------------
-    quit = print("[bold yellow]Type 'quit' to exit the program")
+    quit = print("[bold bright_red]Type 'quit' to exit the program")
     x = input(f"[$]: ")
 
     if x == "1":
@@ -58,6 +59,41 @@ while True:
             console = Console()
             console.print(table)
             pause = input("Press enter to continue... ")
+            
+    if x == "3":
+        print("LOOKUP\n")
+        choice3 = """
+                [bold purple][1]Roblox
+                [bold purple][2]Discord (Really fucking trash rn im to lazy to fix it rn.)
+        """
+        print(choice3)
+        x3 = input(f"[$]: ")
+        
+        if x3 == "1":
+            os.system("cls")
+            userid = input("Enter Roblox UID: ")
+            r = requests.get(f"https://users.roblox.com/v1/users/{userid}")
+            data = r.json()
+            
+            # info
+            box = Panel.fit(renderable= f"[bold purple]Username: {data['name']}\nDisplay Name: {data['displayName']}\nCreated: {data['created']}\nDescription: {data['description']}\nBanned: {data['isBanned']}\nVerified {data['hasVerifiedBadge']}\nCreated: {data['created']}", title = f"User: {data['id']}", border_style="purple", box=HEAVY)
+            print(box)
+            
+            # pause
+            pause = input("Press enter to continue...")
+            
+        if x3 == "2":
+            os.system("cls")
+            userid = input("Enter Discord UID: ")
+            r = requests.get(f"https://api.cord.cat/api/v1/query/{userid}")
+            data = r.json()
+            
+            # info
+            box = Panel.fit(renderable= f"[bold purple]Userinfo: {data['userInfo']}", border_style="purple", box=HEAVY)
+            print(box)
+            
+            pause = input("Press enter to continue...")
+        
     
     if x == "quit":
         os.system("cls")
